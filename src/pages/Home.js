e0f85c7f6de28295ef2f11221cbc35633d2a5ea4
Pareek-kamlesh <kamleshpareek21@gmail.com>
2025-05-29 19:03:14 +0530

    home products updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import './Home.css'; // Create this if not using Tailwind
-import product1 from '../assets/product1.jpg';
-import product2 from '../assets/product2.jpg';
-import product3 from '../assets/product3.jpg';
-import product4 from '../assets/product4.jpg';
-import product5 from '../assets/product5.jpg';
-import product6 from '../assets/product6.jpg';
-import product7 from '../assets/product7.jpg';
-import product8 from '../assets/product8.jpg';
-import product9 from '../assets/product9.jpg';
-import product10 from '../assets/product10.jpg';
+import p1 from '../assets/p1.jpg';
+import p2 from '../assets/p2.jpg';
+import p3 from '../assets/p3.jpg';
+import p4 from '../assets/p4.jpg';
+import p5 from '../assets/p5.jpg';
+import p6 from '../assets/p6.jpg';
+import p7 from '../assets/p7.jpg';
+import p8 from '../assets/p8.jpg';
+import p9 from '../assets/p9.jpg';
+import p10 from '../assets/p10.jpg';
+import p11 from '../assets/p11.jpg';
+import p12 from '../assets/p12.jpg';
+import p13 from '../assets/p13.jpg';
+import p14 from '../assets/p14.jpg';
+import p15 from '../assets/p15.jpg';
 
 const products = [
-  { src: product1, title: 'ST Protein' },
-  { src: product2, title: 'ST Protein- SF' },
-  { src: product3, title: 'Stano Calcium' },
-  { src: product4, title: 'B-Comlex' },
-  { src: product5, title: 'Calcitan-B12' },
-  { src: product6, title: 'Instanfy-Plus' },
-  { src: product7, title: 'IRO MOZ' },
-  { src: product8, title: 'Irotan' },
-  { src: product9, title: 'Tomzyme' },
-  { src: product10, title: 'Stanzyme' },
+  { src: p1},
+  { src: p2},
+  { src: p3 },
+  { src: p4},
+  { src: p5 },
+  { src: p6},
+  { src: p7},
+  { src: p8},
+  { src: p9},
+  { src: p10},
+  { src: p11},
+  { src: p12},
+  { src: p13},
+  { src: p14},
+  { src: p15}
 ];
 
 const Home = () => {
@@ -34,7 +44,7 @@ const Home = () => {
         {products.map((product, index) => (
           <div className="product-card" key={index}>
             <img src={product.src} alt={product.title} />
-            <h4>{product.title}</h4>
+            
           </div>
         ))}
       </div>
